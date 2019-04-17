225c62cbbc8545c9a2c6de0fe80e43b4f045b549
Sungil Saul Lee <jangsoo86@gmail.com>
2019-04-17 16:43:46 +0000

    fetching tweets with AJAX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,52 +5,6 @@
  */
 // Test / driver code (temporary). Eventually will get this from the server.
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
 
 // create a function that creates tweet elements
 function createTweetElement (tweetData) {
@@ -92,24 +46,33 @@ function createTweetElement (tweetData) {
   $("#tweets-container").append(newTweet);
 
   return newTweet;
-}
+};
 
 function renderTweets(tweets) {
   // loops through tweets
-  for (let i = 0; i < data.length; i++) {
-    var $tweet = createTweetElement(data[i]);
+  for (let i = 0; i < tweets.length; i++) {
+    var $tweet = createTweetElement(tweets[i]);
     // append tweet
     $("#tweets-container").append($tweet);
   }
 };
 
+//fetching tweets with AJAX
+function loadTweets() {
+  $.getJSON("/tweets", function(data) {
+    renderTweets(data);
+  })
+};
+
 //make the document ready.
 $(document).ready(function() {
 
+  loadTweets();
   $(".submit-tweet").on("submit", function() {
     event.preventDefault();
     let text = $(this).serialize();
+    // AJAX POST request.
     $.post("/tweets", text);
   })
-  renderTweets(data)
+
 });
