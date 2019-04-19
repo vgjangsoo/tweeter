969af073b8d168fe03779babec1be433dd4073aa
Sungil Saul Lee <jangsoo86@gmail.com>
2019-04-19 20:15:29 +0000

    Fix mongo connection string issue

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 "use strict";
 
 // Basic express setup:
-
+require('dotenv').config();
 const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
 const {MongoClient} = require("mongodb");
-const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+const MONGODB_URI = process.env.MONGODB_URI;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,19 +15,16 @@ app.use(express.static("public"));
 
 //use Mongdo DB
 MongoClient.connect(MONGODB_URI, (err, db) => {
-
+  // if error happens
   if (err) {
     console.error(`Failed to connect: ${MONGODB_URI}`);
     throw err;
   }
-
   const DataHelpers = require("./lib/data-helpers.js")(db);
-
   const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
   // Mount the tweets routes at the "/tweets" path prefix:
   app.use("/tweets", tweetsRoutes);
-
 });
 
 
